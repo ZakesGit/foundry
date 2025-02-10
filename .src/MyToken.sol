1c999dc8c0290f5f5f8bf645a9580ddfa4f08427
ZakesGit <Zakhelecrypto@gmail.com>
2025-02-10 13:06:15 +0200

    Update MyToken.sol
    Our token burns a small fee on every transfer. To do so, we'll add a few more constants and then override the default _update function in OpenZeppelin's ERC20.sol
    
    Signed-off-by: ZakesGit <Zakhelecrypto@gmail.com>

--- .src/MyToken.sol
+++ .src/MyToken.sol
@@ -12,3 +12,23 @@ contract MyToken is ERC20 {
         _mint(msg.sender, INITIAL_SUPPLY);
     }
 }
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20 {
+    uint256 public constant INITIAL_SUPPLY = 1_000_000 * 1 ether;
+    uint256 public constant BURN_PERCENTAGE = 1; // 1%
+    address public constant BURN_ADDRESS = 0xDeaDbeefdEAdbeefdEadbEEFdeadbeEFdEaDbeeF; 
+
+    constructor() ERC20("MyToken", "MT"){
+        _mint(msg.sender, INITIAL_SUPPLY);
+    }
+
+    function _update(address sender, address recipient, uint256 amount) internal override { 
+        uint256 burnAmount = (amount * BURN_PERCENTAGE) / 100;
+        super._update(sender, recipient, amount - burnAmount);
+        super._update(sender, BURN_ADDRESS, burnAmount);
+    }
+}    
