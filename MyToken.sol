bf2b87d87c9ce687fbef48944c6e85358b8505f7
ZakesGit <Zakhelecrypto@gmail.com>
2025-02-10 14:09:01 +0200

    Update MyToken.sol
    Deploy token contract
    
    Signed-off-by: ZakesGit <Zakhelecrypto@gmail.com>

--- MyToken.sol
+++ MyToken.sol
@@ -1,3 +1,6 @@
 # FROM: ./my_token
 
 touch src/MyToken.sol;
+# FROM: ./my_token
+
+forge create --rpc-url https://rpc.berachain.com/ --private-key <rS1ad-YbuD6vKG77ZwTmsaNGC19pmyyN> src/MyToken.sol:MyToken --legacy;
