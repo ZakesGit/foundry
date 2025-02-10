49e7f58ce583fc3b283e16fb9786d659738b0d9b
ZakesGit <Zakhelecrypto@gmail.com>
2025-02-10 12:29:22 +0200

    Update MyToken.sol
    we'll create the actual contract which extends the ERC-20 contract we imported. With this token we will:
    
    Set the name to "MyToken"
    Set the symbol to "MT"
    Set the initial supply to 1,000,000 tokens
    
    Signed-off-by: ZakesGit <Zakhelecrypto@gmail.com>

--- .src/MyToken.sol
+++ .src/MyToken.sol
@@ -2,3 +2,13 @@
 pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol"; 
+
+contract MyToken is ERC20 {
+    uint256 public constant INITIAL_SUPPLY = 1_000_000 * 1 ether;
+
+    constructor() ERC20("MyToken", "MT"){
+        _mint(msg.sender, INITIAL_SUPPLY);
+    }
+}
